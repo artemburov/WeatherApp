d7a719e49428c3c81ab02aa9d0e464982fdf0bfc
Artem Burov <artemburov@list.ru>
2022-06-26 16:41:43 +0400

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from "./components/form"
+import Fetching from "./components/fetching"
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    )
+  }
+}
+
+export default App
